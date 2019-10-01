ddd4c9272901a87d379dbb65494f29a7de2dc5f3
TrevorThomp <trevor.ray.thompson@gmail.com>
2019-09-30 20:27:03 -0700

    refactored render functions

--- js/app.js
+++ js/app.js
@@ -14,14 +14,14 @@ var seattle = {
     },
     cookiesPurchased: function () {
         var result = [];
-        for (var i = 0; i < 16; i++) {
+        for (var i = 0; i < this.storeHours.length; i++) {
             result.push(Math.floor(this.randomNumCustomer() * this.averageCookies));
         }
         return result;
     },
     totalSum: function () {
         var cookieSum = 0;
-        for (var i = 0; i < 16; i++) {
+        for (var i = 0; i <= this.storeHours.length; i++) {
             cookieSum += this.cookiesPurchased()[0];
         }
         return cookieSum.toLocaleString();
@@ -32,7 +32,7 @@ var seattle = {
         ul.className = 'cookies';
         ul.innerHTML = `<h2>${this.location.toUpperCase()}</h2>`;
         section.appendChild(ul);
-        for (var i = 0; i < 14; i++) {
+        for (var i = 0; i <= this.storeHours.length; i++) {
             var li = document.createElement('li');
             li.textContent = `
             ${this.storeHours[i]}: ${this.cookiesPurchased()[i]} cookies
@@ -46,23 +46,23 @@ var seattle = {
 
 var tokyo = {
     location: 'tokyo',
-    minCustomers: 23,
-    maxCustomers: 65,
-    averageCookies: 6.3,
+    minCustomers: 3,
+    maxCustomers: 24,
+    averageCookies: 1.2,
     storeHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
     randomNumCustomer: function() {
         return Math.floor(Math.random() * (this.maxCustomers - this.minCustomers) + this.minCustomers);
     },
     cookiesPurchased: function () {
         var result = [];
-        for (var i = 0; i < 16; i++) {
+        for (var i = 0; i < this.storeHours.length; i++) {
             result.push(Math.floor(this.randomNumCustomer() * this.averageCookies));
         }
         return result;
     },
     totalSum: function () {
         var cookieSum = 0;
-        for (var i = 0; i < 16; i++) {
+        for (var i = 0; i <= this.storeHours.length; i++) {
             cookieSum += this.cookiesPurchased()[0];
         }
         return cookieSum.toLocaleString();
@@ -73,7 +73,7 @@ var tokyo = {
         ul.className = 'cookies';
         ul.innerHTML = `<h2>${this.location.toUpperCase()}</h2>`;
         section.appendChild(ul);
-        for (var i = 0; i < 14; i++) {
+        for (var i = 0; i <= this.storeHours.length; i++) {
             var li = document.createElement('li');
             li.textContent = `
             ${this.storeHours[i]}: ${this.cookiesPurchased()[i]} cookies
@@ -87,23 +87,23 @@ var tokyo = {
 
 var dubai = {
     location: 'dubai',
-    minCustomers: 23,
-    maxCustomers: 65,
-    averageCookies: 6.3,
+    minCustomers: 11,
+    maxCustomers: 38,
+    averageCookies: 3.7,
     storeHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
     randomNumCustomer: function() {
         return Math.floor(Math.random() * (this.maxCustomers - this.minCustomers) + this.minCustomers);
     },
     cookiesPurchased: function () {
         var result = [];
-        for (var i = 0; i < 16; i++) {
+        for (var i = 0; i < this.storeHours.length; i++) {
             result.push(Math.floor(this.randomNumCustomer() * this.averageCookies));
         }
         return result;
     },
     totalSum: function () {
         var cookieSum = 0;
-        for (var i = 0; i < 16; i++) {
+        for (var i = 0; i <= this.storeHours.length; i++) {
             cookieSum += this.cookiesPurchased()[0];
         }
         return cookieSum.toLocaleString();
@@ -114,7 +114,7 @@ var dubai = {
         ul.className = 'cookies';
         ul.innerHTML = `<h2>${this.location.toUpperCase()}</h2>`;
         section.appendChild(ul);
-        for (var i = 0; i < 14; i++) {
+        for (var i = 0; i <= this.storeHours.length; i++) {
             var li = document.createElement('li');
             li.textContent = `
             ${this.storeHours[i]}: ${this.cookiesPurchased()[i]} cookies
@@ -128,23 +128,23 @@ var dubai = {
 
 var paris = {
     location: 'paris',
-    minCustomers: 23,
-    maxCustomers: 65,
-    averageCookies: 6.3,
+    minCustomers: 20,
+    maxCustomers: 38,
+    averageCookies: 2.3,
     storeHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
     randomNumCustomer: function() {
         return Math.floor(Math.random() * (this.maxCustomers - this.minCustomers) + this.minCustomers);
     },
     cookiesPurchased: function () {
         var result = [];
-        for (var i = 0; i < 16; i++) {
+        for (var i = 0; i < this.storeHours.length; i++) {
             result.push(Math.floor(this.randomNumCustomer() * this.averageCookies));
         }
         return result;
     },
     totalSum: function () {
         var cookieSum = 0;
-        for (var i = 0; i < 16; i++) {
+        for (var i = 0; i <= this.storeHours.length; i++) {
             cookieSum += this.cookiesPurchased()[0];
         }
         return cookieSum.toLocaleString();
@@ -155,7 +155,7 @@ var paris = {
         ul.className = 'cookies';
         ul.innerHTML = `<h2>${this.location.toUpperCase()}</h2>`;
         section.appendChild(ul);
-        for (var i = 0; i < 14; i++) {
+        for (var i = 0; i <= this.storeHours.length; i++) {
             var li = document.createElement('li');
             li.textContent = `
             ${this.storeHours[i]}: ${this.cookiesPurchased()[i]} cookies
@@ -169,23 +169,23 @@ var paris = {
 
 var lima = {
     location: 'lima',
-    minCustomers: 23,
-    maxCustomers: 65,
-    averageCookies: 6.3,
+    minCustomers: 2,
+    maxCustomers: 16,
+    averageCookies: 4.6,
     storeHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
     randomNumCustomer: function() {
         return Math.floor(Math.random() * (this.maxCustomers - this.minCustomers) + this.minCustomers);
     },
     cookiesPurchased: function () {
         var result = [];
-        for (var i = 0; i < 16; i++) {
+        for (var i = 0; i < this.storeHours.length; i++) {
             result.push(Math.floor(this.randomNumCustomer() * this.averageCookies));
         }
         return result;
     },
     totalSum: function () {
         var cookieSum = 0;
-        for (var i = 0; i < 16; i++) {
+        for (var i = 0; i <= this.storeHours.length; i++) {
             cookieSum += this.cookiesPurchased()[0];
         }
         return cookieSum.toLocaleString();
@@ -196,7 +196,7 @@ var lima = {
         ul.className = 'cookies';
         ul.innerHTML = `<h2>${this.location.toUpperCase()}</h2>`;
         section.appendChild(ul);
-        for (var i = 0; i < 14; i++) {
+        for (var i = 0; i <= this.storeHours.length; i++) {
             var li = document.createElement('li');
             li.textContent = `
             ${this.storeHours[i]}: ${this.cookiesPurchased()[i]} cookies
